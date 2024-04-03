251c17e34be358ecf6e54abcd9899cc8e2e2a320
nirogu <nirogu@outlook.com>
2024-04-03 13:59:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Neovim configuration
+
+My personal user configuration for neovim. Using [AstroNvim](https://astronvim.com/), and based on its [default template](https://github.com/AstroNvim/template).
+
+## 🛠️ Installation
+
+#### Make a backup of your current nvim configuration and shared folder (or erase them)
+
+```shell
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+```
+
+#### Clone the repository
+
+```shell
+git clone https://github.com/Nirogu/nvim-config ~/.config/nvim
+```
+
+#### Start Neovim
+
+```shell
+nvim
+```
---
+++ lua/community.lua
@@ -0,0 +1,17 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typst" },
+}
